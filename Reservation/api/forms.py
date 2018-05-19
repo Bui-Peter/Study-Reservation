9ad18926c19c8251ee96b08b2c04a25f7acaa767
Bui-Peter <PeterBui98@gmail.com>
2018-05-18 20:00:25 -0400

    modified models

---
+++ Reservation/api/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+def DateForm(forms.Forms):
+    study_date = forms.DateField(label='Study Date:')
--- Reservation/api/views.py
+++ Reservation/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from . import DateForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world. You are at the index")
+
+def get_date(request);
+    if request.method == 'POST':
+        form = DateForm(request.POST)
+
+        if form.is_valid():
+            form.cleaned_data
+            
 
