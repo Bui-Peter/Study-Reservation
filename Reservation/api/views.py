311505cd83ef368b553d5d34099ca1619483bd90
Bui-Peter <PeterBui98@gmail.com>
2018-05-30 10:52:38 -0400

    Begin view-template

--- Reservation/api/views.py
+++ Reservation/api/views.py
@@ -7,7 +7,33 @@ from .forms import DateForm
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world. You are at the index")
+    date = Date(study_date='2018-05-30', room_size=4)
+    date.save()
+
+    result = "You are at the index"
+
+'''
+    try: 
+        response = requests.get('https://gmu.libcal.com/spaces/accessible/ajax/group?prevGroupId=2142&gid=2142&capacity=0', data)
+        html = BeautifulSoup(response.text, 'html.parser')
+
+        try:
+            unavailable = html.find('div', class_='alert alert-warning').contents[0].strip()
+
+            resut = unavailable
+
+        except:
+            for collection in html.find_all('fieldset'):
+                newRoom = collection.legend.contents[0].strip()
+                room[newroom] = []
+                for period in collection.find_all('label'):
+                    room[newRoom].append(period.contents[2].strip())
+
+    except:
+       result = "Unable to retrieve data"
+'''
+
+    return HttpResponse(result)
 
 def get_date(request):
     return HttpResponse("Getting the date")
