6b7aaf765daa04d9d4ae3359aa3913e4330e7e8d
Bui-Peter <PeterBui98@gmail.com>
2018-06-05 12:24:40 -0400

    Add feature to get room sizes with a POST request. Also add default date as current date.

--- Reservation/api/views.py
+++ Reservation/api/views.py
@@ -1,30 +1,38 @@
+import datetime
 import requests
 from bs4 import BeautifulSoup
 
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from django.http import HttpResponseRedirect
 from .forms import DateForm
 from .models import Date
-# Create your views here.
 
 def index(request):
-    date = Date(study_date='2018-05-30', room_size=4)
+    size = 2
+    if request.method == "POST":
+        size = int(request.POST['size'])
+    
+    #default date is today
+    date = Date(study_date=str(datetime.date.today()), room_size=size)
     date.save()
     
     room_list = []
 
-    result = "You are at the index"
-
     #Get rooms
-
-    data = {'date' : date.study_date}
+    #gid:
+    #   lobby(0) = 2142
+    #   2 = 2116
+    #   4 = 2117
+    #   6 = 2118
+    #   8 = 2119
+    gid = {0 : 2142,  2 : 2116, 4 : 2117, 6 : 2118, 8 : 2119}
+    data = {'gid' : gid[date.room_size], 'date' : date.study_date}
     room = {}
     
     
     try:
         #Get the html of all rooms
-        response = requests.get('https://gmu.libcal.com/spaces/accessible/ajax/group?prevGroupid=2142&gid=2142&capacity=0', data)
+        response = requests.get('https://gmu.libcal.com/spaces/accessible/ajax/group?prevGroupid=2142', data)
         html = BeautifulSoup(response.text, 'html.parser')
 
         try:
@@ -46,8 +54,5 @@ def index(request):
     except:
         raise Http404('Unable to retrieve data')
 
-
-    room_list.append("Hi")
     context = {'given_date' : date,'room_list' : room}
     return render(request, 'api/index.html', context)
-
